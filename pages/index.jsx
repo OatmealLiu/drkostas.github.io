0dc4477d48395759cfe9a2814fcae178007db609
miu <mingxuanliu.miu@gmail.com>
2024-07-19 17:31:56 +0200

    init

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,48 +13,49 @@ export default function HomePage() {
         </div>
         <div className={styles.foreground}>
           <div className={styles.content}>
-            <h1 className={styles.name}>Kostas Georgiou</h1>
-            <h6 className={styles.bio}>AI Research Engineer</h6>
+            <h1 className={styles.name}>Mingxuan LIU - Miu</h1>
+            <h6 className={styles.bio}>Second-year PhD Student @UniTrento</h6>
+            <h6 className={styles.bio}>Visiting Researcher @NaverLabsEurope</h6>
             <div className={styles.cardContainer}>
               <div className={styles.card}>
                 <div className={styles.content}>
-                  <h4>Skill Set</h4>
+                  <h4>Research Interests and Skill Set</h4>
                   <div className={styles.tags}>
                     <span key='Research' className='Research'>
-                      Research
+                      Open-world Perception
                     </span>
                     <span key='Computer-Vision' className='Computer-Vision'>
-                      Computer-Vision
+                      Vision-Language
                     </span>
                     <span key='MIM' className='MIM'>
-                      Masked-Image-Modeling
+                      Object Detection
                     </span>
                     <span key='LLMs' className='LLMs'>
                       LLMs
                     </span>
                     <span key='GenAI' className='GenAI'>
-                      Generative AI
+                      Open-vocabulary
                     </span>
                     <span key='PyTorch' className='PyTorch'>
-                      PyTorch
+                      Knowledge Discovery
                     </span>
                     <span key='Tensorflow' className='Tensorflow'>
-                      Tensorflow
+                      Robotics
                     </span>
                     <span key='Apache-Spark' className='Apache-Spark'>
-                      Apache-Spark
+                      Python
                     </span>
                     <span key='SQL' className='SQL'>
-                      SQL
+                      C++
                     </span>
                     <span key='MLOps' className='MLOps'>
-                      MLOps
+                      Unity
                     </span>
                     <span key='Software-Design' className='Software-Design'>
-                      Software-Engineering
+                      Maple
                     </span>
                     <span key='Bots' className='Bots'>
-                      Bots
+                      Piano
                     </span>
                   </div>
                 </div>
@@ -76,8 +77,8 @@ export default function HomePage() {
               <Image
                 className={styles.picture}
                 src="/me.jpeg"
-                width={300}
-                height={300}
+                width={500}
+                height={500}
                 alt="Kostas' Picture"
               />
             </div>
